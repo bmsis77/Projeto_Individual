1d19ae515aa21b1cab5c194bfc4d27ba3005841d
bmsis77 <bryan.silva@sptech.school>
2025-05-21 13:50:02 -0300

    API alterações

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -26,3 +26,71 @@ module.exports = {
     autenticar,
     cadastrar
 };
+
+// console.log("Usuário encontrado: ", resultadoAutenticar[0]);
+// console.log("Autenticação realizada com sucesso!");
+// res.json({
+// id: resultadoAutenticar[0].idusuario,
+// email: resultadoAutenticar[0].email,
+// username: resultadoAutenticar[0].username,
+// senha: resultadoAutenticar[0].senha,
+// genero: resultadoAutenticar[0].genero
+// });
+// } else if (resultadoAutenticar.length == 0) {
+// res.status(403).send("Email e/ou senha inválido(s)");
+// } else {
+// res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+// }
+// }
+// ).catch(
+// function (erro) {
+// console.log(erro);
+// console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+// res.status(500).json(erro.sqlMessage);
+// }
+// );
+// }
+
+// }
+
+// function cadastrar(req, res) {
+// // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+// var username = req.body.usernameServer;
+// var email = req.body.emailServer;
+// var senha = req.body.senhaServer;
+// var genero = req.body.generoServer;
+
+// // Faça as validações dos valores
+// if (username == undefined) {
+// console.log("Seu nome está undefined!");
+// } else if (email == undefined) {
+// console.log("Seu email está undefined!");
+// } else if (senha == undefined) {
+// console.log("Sua senha está undefined!");
+// } else if (genero == undefined) {
+// console.log("Sua Seu gênero está undefined!");
+// } else {
+
+// // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+// usuarioModel.cadastrar(username, email, senha, genero)
+// .then(
+// function (resultado) {
+// res.json(resultado);
+// }
+// ).catch(
+// function (erro) {
+// console.log(erro);
+// console.log(
+// "\nHouve um erro ao realizar o cadastro! Erro: ",
+// erro.sqlMessage
+// );
+// res.status(500).json(erro.sqlMessage);
+// }
+// );
+// }
+// }
+
+// module.exports = {
+// autenticar,
+// cadastrar
+// }
